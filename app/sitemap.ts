2c4a727f368f47a54036bfb8ee038c90fd04374d
audrew <audriusg36@gmail.com>
2026-05-03 02:37:14 +0300

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,16 +9,64 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: "https://linksmauodegele.lt/#globotiniai",
+      url: "https://linksmauodegele.lt/gyvunai",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.9,
     },
     {
-      url: "https://linksmauodegele.lt/#naujienos",
+      url: "https://linksmauodegele.lt/apie",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://linksmauodegele.lt/naujienos",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+    {
+      url: "https://linksmauodegele.lt/parama",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://linksmauodegele.lt/parama/finansine",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: "https://linksmauodegele.lt/parama/gpm",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: "https://linksmauodegele.lt/parama/daiktai",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: "https://linksmauodegele.lt/parama/isigyk",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.7,
+    },
+    {
+      url: "https://linksmauodegele.lt/rado-namus",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.7,
+    },
+    {
+      url: "https://linksmauodegele.lt/kontaktai",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.6,
     },
   ];
 }
